12cb053491a13bfa643133ca401df0f510f92a89
Carlos Santos <karscx@gmail.com>
2024-07-17 17:06:44 -0300

    Deploy

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");const bodyParser = require("body-parser");
+const { exec } = require("child_process");
+const path = require("path");
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/deploy", (req, res) => {
+  exec("bash deploy.sh", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error ejecutando el comando: ${error}`);
+      return res
+        .status(500)
+        .send(`Error ejecutando el comando: ${error.message}`);
+    }
+    if (stderr) {
+      console.error(`Error en el comando: ${stderr}`);
+      return res.status(500).send(`Error en el comando: ${stderr}`);
+    }
+    console.log(`Resultado del comando: ${stdout}`);
+    res.send(`Comando ejecutado con éxito:\n${stdout}`);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
